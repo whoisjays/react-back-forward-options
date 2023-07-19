8dfb1015dea497064d0fee94758d13e51e2933f3
whoisjays <djince.d@gmail.com>
2023-07-19 16:15:17 +0300

    test: refactor

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,11 +1,10 @@
-import { render, screen } from "@testing-library/react";
-import React from "react";
-import { BackForward } from "./index";
+import {render, screen} from '@testing-library/react';
+import {BackForward} from './index';
 
-describe("<BackForward/>", () => {
-  it("should render back forward", () => {
+describe('<BackForward/>', () => {
+  it('should render back forward', () => {
     render(<BackForward />);
 
-    expect(screen.getByText("BackForward")).toBeVisible();
+    expect(screen.getByText('BackForward')).toBeVisible();
   });
 });
