a4c530b0fe4f196ea7fd5e0f4076bf8e51156119
whoisjays <djince.d@gmail.com>
2023-07-23 13:50:17 +0300

    refactor: deleted unnecessary component

--- src/components/BackForward/BackForward.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import * as Styled from './BackForward.styled';
-
-export const BackForward: React.FC = () => {
-  return <Styled.Container>BackForward</Styled.Container>;
-};
